0a4216a2390244ee2f5f01e68251f77433175713
keegangm <kgmorris76@gmail.com>
2023-03-28 18:51:51 -0500

    feat: final commit

--- number_guess.sh
+++ number_guess.sh
@@ -6,7 +6,8 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 # setup dialogue
 
   # initial dialogue: Enter your username and then read username
-  echo "Enter your username:"
+  echo -e "~~ NUMBER GUESSING GAME ~~\n"
+  echo Enter your username:
   read USERNAME
 
   # lookup username
@@ -16,10 +17,11 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
   then
 
     # echo message
-    echo "Welcome, $USERNAME! It looks like this is your first time here."
+    echo Welcome, $USERNAME! It looks like this is your first time here.
 
     # insert new user into users table
     INSERT_NEW_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+    LOOKUP_USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 
   else
     # lookup user_id, user_games, and best_game from users and games tables
@@ -28,7 +30,7 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
     LOOKUP_BEST_GAME=$($PSQL "SELECT min(loops_till_win) FROM games WHERE user_id=$LOOKUP_USER_ID")
 
    # print Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses
-    echo "Welcome back, $USERNAME! You have played $LOOKUP_USER_GAMES games, and your best game took $LOOKUP_BEST_GAME guesses."
+    echo Welcome back, $USERNAME! You have played $LOOKUP_USER_GAMES games, and your best game took $LOOKUP_BEST_GAME guesses.
   fi
 
 # game dialogue
@@ -38,29 +40,39 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
   # generate SECRET_NUMBER
   SECRET_NUMBER=$(( ( RANDOM % 1000 ) +1 ))
+  # test: echo $SECRET_NUMBER
 
   # insert $SECRET_NUMBER into games log table
   UPDATE_SECRET_NUMBER=$($PSQL "UPDATE games SET secret_number=$SECRET_NUMBER WHERE game_id=$EXTRACT_GAME_ID")
 
   # echo "Guess the secret number between 1 and 1000:"
-  echo "Guess the secret number between 1 and 1000:"
+  echo Guess the secret number between 1 and 1000:
 
-  # loop
-    
-    # read GUESS_NUMBER
-    read GUESS_NUMBER
+  while true
+  do
+  ((GUESSES++))
 
-    # if GUESS_NUMBER < SECRET_NUMBER, 
-    # then echo "It's higher than that, guess again:"
+  read GUESS
 
-    # if GUESS_NUMBER > SECRET_NUMBER
-    # then echo "It's lower than that, guess again:"
+  if  [[ ! $GUESS =~ ^[0-9]+$ ]]
+  then
+    echo That is not an integer, guess again:
+  elif [[ $GUESS -gt $SECRET_NUMBER ]]
+  then
+    echo It\'s lower than that, guess again:
+  elif [[ $GUESS -lt $SECRET_NUMBER ]]
+  then
+    echo It\'s higher than that, guess again:
+  elif [[ $GUESS -eq $SECRET_NUMBER ]]
+  then 
+    #insert guesses
+    INSERT_GUESSES=$($PSQL "UPDATE games SET loops_till_win=$GUESSES WHERE game_id=$EXTRACT_GAME_ID")
+    echo You guessed it in $GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!
+    exit
 
-    # if GUESS_NUMBER not an integer
-    # then echo "That is not an integer, guess again:"
+  fi
 
-    # if GUESS_NUMBER=SECRET_NUMBER
-    # echo "You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. Nice job!"
+  done
 
 
 # NEEDS: SOME WAY TO LOG # OF LOOPS
